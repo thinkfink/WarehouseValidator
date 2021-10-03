337bda6d56f5dbc0fce13ec4a32a987c55855d2e
Tyler <tylerfink95@gmail.com>
2021-10-03 17:07:01 -0500

    Created LicensePlate class

--- WarehouseValidator/FrmWarehouseValidator.cs
+++ WarehouseValidator/FrmWarehouseValidator.cs
@@ -19,7 +19,22 @@ namespace WarehouseValidator
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
+            if (txtScan.Text.StartsWith("A") || txtScan.Text.StartsWith("a"))
+            {
 
+            }
+            else if (txtScan.Text.StartsWith("P") || txtScan.Text.StartsWith("p"))
+            {
+
+            }
+            else if (txtScan.Text.StartsWith("Q") || txtScan.Text.StartsWith("q"))
+            {
+
+            }
+            else
+            {
+                
+            }
         }
     }
 }
---
+++ WarehouseValidator/Models/LicensePlate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseValidator.Models
+{
+    public class LicensePlate
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PartNumber { get; set; }
+        public string PartDescription { get; set; }
+        public string Qty { get; set; }
+        public string LotNumber { get; set; }
+        public string ReceiptNumber { get; set; }
+        public string LocationName { get; set; }
+        public static LicensePlate FromCSV(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            LicensePlate licensePlate = new LicensePlate();
+            licensePlate.Id = Guid.NewGuid();
+            licensePlate.Name = values[1].ToString();
+            licensePlate.LotNumber = values[2].ToString();
+            licensePlate.PartNumber = values[3].ToString();
+            licensePlate.PartDescription = values[4].ToString();
+            licensePlate.Qty = values[5].ToString();
+            licensePlate.LocationName = values[6].ToString();
+            return licensePlate;
+        }
+    }
+}
